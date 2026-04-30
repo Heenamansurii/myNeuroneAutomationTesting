641dcb0ac98d62d758965cf3b50b27201d3ac336
Heena Mansuri <heenamansuri318@gmail.com>
2026-04-30 17:14:29 +0530

    Initial commit

---
+++ src/test/java/testcase/register/DashboardButtonTest.java
@@ -0,0 +1,57 @@
+package testcase.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class DashboardButtonTest {
+
+	@Test
+	public void verifyAllButton() throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
+		driver.get("https://myneuron.leadows.com/auth/login");
+		driver.findElement(By.name("email")).sendKeys("heenaleadows@gmail.com");
+		driver.findElement(By.name("password")).sendKeys("Password@123");
+		driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+		// right tab click check
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[1]/button/span")).click(); // three line
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/aside/div/ul/li[2]")).click(); // GATC
+		driver.findElement(By.xpath("//a[text()='GATC 2026']")).click(); // GATC 2026
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/aside/div/ul/li[3]/span/a")).click(); // my bookshelf
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div/aside/div/ul/li[4]/span/a")).click(); // impulse
+
+		// left side button click check
+
+//		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[3]/div[1]/button[5]")).click(); // notification
+//
+//		// close notification
+//		WebElement closeBtn = driver.findElement(By.cssSelector(".ant-drawer-close"));
+//
+//		((org.openqa.selenium.JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", closeBtn);
+//
+//		closeBtn.click();
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[3]/div[1]/button[4]")).click(); // chat
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[3]/div[1]/button[3]")).click(); // book shelf
+																											// icon
+		//Thread.sleep(20);
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[3]/div[1]/button[2]")).click(); // impulse icon
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/header/div[3]/div[1]/button[1]")).click(); // home button
+
+
+   
+			 driver.quit();
+
+	}
+}
---
+++ src/test/java/testcase/register/RegisteUsingTab.java
@@ -0,0 +1,80 @@
+package testcase.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import utils.CommonUtils;
+
+public class RegisteUsingTab {
+
+	WebDriver driver;
+
+	@AfterMethod
+	public void tearDown() {
+	//	driver.quit();
+
+	}
+
+	@Test
+		public void tabUsing() throws InterruptedException {
+			driver =new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+			
+			driver.get("https://myneuron.leadows.com/auth/register/");
+			
+		    Actions actions = new Actions(driver);
+		  
+//		  for(int i=0;i<=4; i++) 
+//		  {
+//			  actions.sendKeys(Keys.TAB).perform();		
+//			  		  
+//		  }	
+
+		    // 🔹 Click on first field to start typing
+		    WebElement firstName = driver.findElement(By.name("first_name"));
+		    firstName.click();
+
+		    // 🔹 Start filling form using TAB
+		    actions
+		        .sendKeys("heena")                  // First Name
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys("")                       // Middle Name (skip)
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys("mansuri")                // Last Name
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys(CommonUtils.generateBrandNewMail()) // Email
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys("Password@123")           // Password
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys("Password@123")           // Confirm Password
+		        .sendKeys(Keys.TAB)
+
+		        .sendKeys(Keys.SPACE)               // Select checkbox (terms)
+		      //  .sendKeys(Keys.TAB)
+
+		        .sendKeys(Keys.ENTER)               // Submit form
+		        .build()
+		        .perform();
+
+		    Thread.sleep(3000);
+		  // driver.quit();
+		}
+}
+		
+		
+
+
